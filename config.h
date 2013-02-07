193a1a12c19b4e13073c6d2421c469d357d94dc2
Aaron Hamilton <aaron@correspondwith.me>
2013-02-07 17:24:59 -0500

    added some useful information to config.h

--- config.h
+++ config.h
@@ -56,40 +56,39 @@ static const char *lockcmd[]  = { "slock", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_o,      spawn,          {.v = lockcmd } },
-/*	{ MODKEY,                       XK_b,      togglebar,      {0} }, */
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },        /* Start dmenu_run in the way defined in dmenucmd[] */
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },         /* Start your terminal as defined in termcmd[] */
+	{ MODKEY,                       XK_o,      spawn,          {.v = lockcmd } },         /* Start your lock command, as defined in lockcmd[] */
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },              /* Bring focused window up in the floating stack */
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },              /* Push focused window down in the floating stack */
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },              /* Increment the number of windows in the master column(left column) */
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },              /* Decrement the number of windows in the master column */
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },            /* Decrease the spatial dominance of the master column */
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },            /* Increase the spatial dominance of the master column */
+	{ MODKEY,                       XK_z,      zoom,           {0} },                     /* zoom */
+	{ MODKEY,                       XK_Tab,    view,           {0} },                     /* Switch between the currently and previously-visible tag */
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },                     /* Close a window */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },      /* Use layout 0 (Tile), as defined in layouts[]  */
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },      /* Use layout 1 (Floating), as defined in layouts[]  */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },      /* Use layout 2 (Monocle), as defined in layouts[]  */
+	{ MODKEY,                       XK_space,  setlayout,      {0} },                     /* Switch to tiling layout */
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },                     /* Toggle floating on current window */
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },             /* Prefix for switching viewed tags */
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },             /* Prefix for tagging a window */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },              /* Switch monitor focus backward(usually to the left) */
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },              /* Switch monitor focus forward(usually to the right) */
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },              /* Tag focused window backward in the monitor stack */
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },              /* Tag focused window forward in the monitor stack */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_4,                      3)                         /* Tagging keys, all number keys by default. Go ahead and add more tags, or remove them if you'd like. */
 	TAGKEYS(                        XK_5,                      4)
 	TAGKEYS(                        XK_6,                      5)
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },                    /* Quit */
 };
 
 /* button definitions */
