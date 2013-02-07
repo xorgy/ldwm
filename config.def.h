35f8befa0df58ff185b9e13ace5c6faae4ee0e90
Aaron Hamilton <aaron@correspondwith.me>
2013-02-07 17:24:59 -0500

    Explain keybindings and the snap variable in config.h

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@ static const char normbgcolor[]     = "#222222";
 static const char normfgcolor[]     = "#bbbbbb";
 static const char selbgcolor[]      = "#de9700";
 static const char selfgcolor[]      = "#eeeeee";
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 32;       /* monitor edge snap distance */
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
@@ -53,41 +53,41 @@ static const char *qwercmd[]  = { "setxkbmap", "us", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_a,      spawn,          {.v = dvorcmd } },
-	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = qwercmd } },
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_o,      spawn,          {.v = lockcmd } },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05 } },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05 } },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_a,      spawn,          {.v = dvorcmd } },     /* Set X keyboard layout to Dvorak*/
+	{ MODKEY|ShiftMask,             XK_a,      spawn,          {.v = qwercmd } },     /* Set X keyboard layout to US */
+	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },    /* Run dmenucmd[] */
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },     /* Run termcmd[] */
+	{ MODKEY,                       XK_o,      spawn,          {.v = lockcmd } },     /* Run lockcmd[] */
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },          /* Bring focused window up in the floating stack */
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },          /* Push focused window down in the floating stack */
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },          /* Increment the number of windows in the master column(left column) */
+	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },          /* Decrement the number of windows in the master column */
+	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05 } },       /* Decrease the spatial dominance of the master column */
+	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05 } },       /* Increase the spatial dominance of the master column */
+	{ MODKEY,                       XK_z,      zoom,           {0} },                 /* Put the focused window into the master column */
+	{ MODKEY,                       XK_Tab,    view,           {0} },                 /* Switch between the currently and previously-visible tag */
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },                 /* Close the focused window */
+	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },  /* Use layout 0 (Tile), layouts[]  */
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },  /* Use layout 1 (Floating), layouts[]  */
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },  /* Use layout 2 (Monocle), layouts[]  */
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },                 /* Toggle floating on current window */
+	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },         /* Prefix for switching viewed tags */
+	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },         /* Prefix for tagging a window */
+	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },          /* Focus the previous monitor */
+	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },          /* Focus the next monitor */
+	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },          /* Tag the focused window to the previous monitor */
+	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },          /* Tag the focused window to the next monitor */
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
+	TAGKEYS(                        XK_4,                      3)                     /* Tagging keys */
 	TAGKEYS(                        XK_5,                      4)
 	TAGKEYS(                        XK_6,                      5)
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },                 /* Quit */
 };
 
 /* button definitions */
