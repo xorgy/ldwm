9a535a8162edf82c7cf7f6c774da4929d83eff88
Aaron Muir Hamilton <aaron@correspondwith.me>
2013-11-21 04:45:53 +0000

    Add minix3 version of config.mk

---
+++ config.minix3.mk
@@ -0,0 +1,24 @@
+# ldwm version
+VERSION = 6-devel
+
+# Customize below to fit your system
+
+# paths
+PREFIX = /usr/pkg
+MANPREFIX = ${PREFIX}/man
+
+X11INC = /usr/pkg/X11R6/include
+X11LIB = /usr/pkg/X11R6/lib
+
+# includes and libs
+INCS = -I${X11INC}
+LIBS = -L${X11LIB} -lX11
+
+# flags
+CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
+#CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+LDFLAGS  = -s ${LIBS}
+
+# compiler and linker
+CC = cc
