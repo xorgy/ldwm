3daa25d109a424562374e71ddc4ea0cf1141297f
Aaron Muir Hamilton <aaron@correspondwith.me>
2013-11-22 16:59:27 +0000

    Disable warnings for deprecated declarations, Xorg will never drop this and it's not hurting anyone.

--- config.minix3.mk
+++ config.minix3.mk
@@ -17,7 +17,7 @@ LIBS = -L${X11LIB} -lX11
 # flags
 CPPFLAGS = -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\"
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = -s ${LIBS}
 
 # compiler and linker
