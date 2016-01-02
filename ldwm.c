e693dfbf9b019ad2ddd96c7b7d03e3753804ab82
Aaron Hamilton <aaron@correspondwith.me>
2016-01-02 21:24:51 +0000

    Throttle to 120ths of a second rather than 60ths.

--- ldwm.c
+++ ldwm.c
@@ -940,7 +940,7 @@ movemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 120))
 				continue;
 			lasttime = ev.xmotion.time;
 
@@ -1074,7 +1074,7 @@ resizemouse(const Arg *arg)
 			handler[ev.type](&ev);
 			break;
 		case MotionNotify:
-			if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+			if ((ev.xmotion.time - lasttime) <= (1000 / 120))
 				continue;
 			lasttime = ev.xmotion.time;
 
